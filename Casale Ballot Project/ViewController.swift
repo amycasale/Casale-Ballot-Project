824627a8a65a2da189bd2561233f478148247987
Amy Casale <a.casale@setonhill.edu>
2020-03-24 23:17:58 -0400

    Initial Commit

---
+++ Casale Ballot Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Casale Ballot Project
+//
+//  Created by Amy Casale on 3/24/20.
+//  Copyright © 2020 Amy Casale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
